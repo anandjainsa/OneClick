9983190f300c2d8860cc683ab31e7f9159d241e8
anandjainsa <53891867+anandjainsa@users.noreply.github.com>
2020-05-31 04:32:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "null_resource" "web" {
   # ...
 
   provisioner "local-exec" {
-    command = "echo 'Hello World' >> private_ips.txt"
+    command = "echo 'Hello World' >> /tmp/my_private_ips.txt"
   }
 }
